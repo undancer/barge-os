f400b2291b8d71ce1ab767808bcfc7565c5898fd
undancer <undancer+github@gmail.com>
2023-05-23 22:27:59 +0800

    login

--- earthly.sh
+++ earthly.sh
@@ -6,7 +6,7 @@ docker run \
   --rm \
   -t \
   --env EARTHLY_BUILD_ARGS \
-  -e EARTHLY_TOKEN=WrVYcaLzZjfnrykF94VDYSE7ACteDlKma7KjfJbTZj0I9kUXzmWjVkHC5nu0KvJy
+  -e EARTHLY_TOKEN=WrVYcaLzZjfnrykF94VDYSE7ACteDlKma7KjfJbTZj0I9kUXzmWjVkHC5nu0KvJy \
   -v /var/run/docker.sock:/var/run/docker.sock \
   -v "$(pwd)":/workspace \
   -v earthly-tmp:/tmp/earthly:rw \
