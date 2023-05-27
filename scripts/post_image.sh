ab0cc47181d78de8b34f6ac6e093ec92e22e9c67
undancer <undancer+github@gmail.com>
2023-05-27 20:03:27 +0800

    syslinux-11

--- scripts/post_image.sh
+++ scripts/post_image.sh
@@ -37,49 +37,49 @@ xorriso \
   # -b boot/isolinux/isolinux.bin \
   # -isohybrid-mbr /usr/lib/syslinux/isohdpfx.bin \
 
-# Make a bootable disk image
-IMAGE=${IMAGES}/barge.img
-DISK=${IMAGES}/disk
-ISO=${IMAGES}/ISO
+# # Make a bootable disk image
+# IMAGE=${IMAGES}/barge.img
+# DISK=${IMAGES}/disk
+# ISO=${IMAGES}/ISO
 
-df -h .
+# df -h .
 
-mkdir -p ${ISO}
-loop0=$(losetup -f)
-losetup $loop0 ${IMAGES}/barge.iso
-mount $loop0 ${ISO}
+# mkdir -p ${ISO}
+# loop0=$(losetup -f)
+# losetup $loop0 ${IMAGES}/barge.iso
+# mount $loop0 ${ISO}
 
-SIZE=$(du -s ${ISO} | awk '{print $1}')
+# SIZE=$(du -s ${ISO} | awk '{print $1}')
 
-dd if=/dev/zero of=${IMAGE} bs=1024 count=$((${SIZE}+68+${SIZE}%2))
-loop1=$(losetup -f)
-losetup $loop1 ${IMAGE}
-(echo c; echo n; echo p; echo 1; echo; echo; echo t; echo 4; echo a; echo w;) | fdisk $loop1 || true
+# dd if=/dev/zero of=${IMAGE} bs=1024 count=$((${SIZE}+68+${SIZE}%2))
+# loop1=$(losetup -f)
+# losetup $loop1 ${IMAGE}
+# (echo c; echo n; echo p; echo 1; echo; echo; echo t; echo 4; echo a; echo w;) | fdisk $loop1 || true
 
-loop2=$(losetup -f)
-losetup -o 32256 $loop2 ${IMAGE}
-mkfs -t vfat -F 16 $loop2
+# loop2=$(losetup -f)
+# losetup -o 32256 $loop2 ${IMAGE}
+# mkfs -t vfat -F 16 $loop2
 
-mkdir -p ${DISK}
-mount -t vfat $loop2 ${DISK}
+# mkdir -p ${DISK}
+# mount -t vfat $loop2 ${DISK}
 
-mkdir -p ${DISK}/boot/syslinux
-cp ${ISO}/boot/bzImage ${DISK}/boot/
-cp ${ISO}/boot/initrd ${DISK}/boot/
-cp ${ISO}/boot/isolinux/isolinux.cfg ${DISK}/boot/syslinux/syslinux.cfg
-umount ${ISO}
-umount ${DISK}
+# mkdir -p ${DISK}/boot/syslinux
+# cp ${ISO}/boot/bzImage ${DISK}/boot/
+# cp ${ISO}/boot/initrd ${DISK}/boot/
+# cp ${ISO}/boot/isolinux/isolinux.cfg ${DISK}/boot/syslinux/syslinux.cfg
+# umount ${ISO}
+# umount ${DISK}
 
-syslinux -i -d /boot/syslinux $loop2 2> ${IMAGES}/error.log
-cat ${IMAGES}/error.log >&2
-losetup -d $loop2
-dd if=/usr/lib/syslinux/mbr.bin of=$loop1 bs=440 count=1
-losetup -d $loop1
-losetup -d $loop0
+# syslinux -i -d /boot/syslinux $loop2 2> ${IMAGES}/error.log
+# cat ${IMAGES}/error.log >&2
+# losetup -d $loop2
+# dd if=/usr/lib/syslinux/mbr.bin of=$loop1 bs=440 count=1
+# losetup -d $loop1
+# losetup -d $loop0
 
-if [ -s ${IMAGES}/error.log ]; then
-  echo "----------SIGN----------"
-  cat ${IMAGES}/error.log
-  echo "----------SIGN----------"
-  exit 1
-fi
+# if [ -s ${IMAGES}/error.log ]; then
+#   echo "----------SIGN----------"
+#   cat ${IMAGES}/error.log
+#   echo "----------SIGN----------"
+#   exit 1
+# fi
