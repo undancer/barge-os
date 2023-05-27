361529451bcc229d87c69d46bcc5bbec1c3e5af0
undancer <undancer+github@gmail.com>
2023-05-27 15:45:58 +0800

    syslinux-10

--- scripts/post_image.sh
+++ scripts/post_image.sh
@@ -29,7 +29,7 @@ xorriso \
   -as mkisofs \
   -l -J -R -V "BARGE" \
   -no-emul-boot -boot-load-size 4 -boot-info-table \
-  -b ${ISO}/boot/isolinux/isolinux.bin \
+  -b boot/isolinux/isolinux.bin \
   -c boot/isolinux/boot.cat \
   -isohybrid-mbr /usr/lib/ISOLINUX/isohdpfx.bin \
   -no-pad -o ${IMAGES}/barge.iso $(pwd)
